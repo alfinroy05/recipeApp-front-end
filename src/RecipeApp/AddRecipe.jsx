6f283d8bfe20e7035e526f5df9ab00a6710b8010
alfinroy05 <alfinroy2005@gmail.com>
2024-04-02 20:45:35 +0530

    Add recipe created

--- src/RecipeApp/AddRecipe.jsx
+++ src/RecipeApp/AddRecipe.jsx
@@ -6,7 +6,7 @@ const AddRecipe = () => {
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <div className="row g-3">
+                    <div className="row g-3git add">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">ENTER TITLE</label>
                             <input type="text" className="form-control" />
