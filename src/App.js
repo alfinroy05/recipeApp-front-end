238b34a6a363fb45ebc008c5aedb9c3e7c1206c7
alfinroy05 <alfinroy2005@gmail.com>
2024-04-02 20:25:14 +0530

    Search recipe created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './RecipeApp/AddRecipe';
+import SearchRecipe from './RecipeApp/SearchRecipe';
 
 function App() {
   return (
     
     <div>
       <AddRecipe/>
+      <SearchRecipe/>
 
     </div>
   );
