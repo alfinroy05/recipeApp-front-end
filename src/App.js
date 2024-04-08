481bf73d313ef918cb0f91a48c8bffff36cca3b1
alfinroy05 <alfinroy2005@gmail.com>
2024-04-08 21:02:05 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 import AddRecipe from './RecipeApp/AddRecipe';
 import SearchRecipe from './RecipeApp/SearchRecipe';
 
+import View from './RecipeApp/View';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+
+
+
+
 function App() {
   return (
-    
-    <div>
-      <AddRecipe/>
-      <SearchRecipe/>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<AddRecipe/>}/>
+    <Route path='/search' element={<SearchRecipe/>}/>
+    <Route path='/view' element={<View/>}/>
+  </Routes>
+  </BrowserRouter>
+     
+       
 
-    </div>
+    
   );
 }
 
